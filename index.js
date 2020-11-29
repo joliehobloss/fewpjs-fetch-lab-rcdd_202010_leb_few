705cac0796b7a47f5507c1377525576a346af707
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-29 12:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function fetchBooks() {
 
 fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => console.log(json));
+  .then(json => renderBooks(json));
 
 
 }
